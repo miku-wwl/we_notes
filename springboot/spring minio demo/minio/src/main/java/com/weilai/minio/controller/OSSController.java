8f7715fe957dd9b69593a838625b7ff264e1451d
miku-wwl <569144003@qq.com>
2024-11-07 22:26:13 +0800

    notes: Spring Boot integrates MinIO

---
+++ springboot/spring minio demo/minio/src/main/java/com/weilai/minio/controller/OSSController.java
@@ -0,0 +1,100 @@
+package com.weilai.minio.controller;
+
+import com.weilai.minio.config.MinioConfig;
+import com.weilai.minio.util.MinioUtils;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.tomcat.util.http.fileupload.IOUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.InputStream;
+
+@Slf4j
+@RestController
+@RequestMapping("/oss")
+public class OSSController {
+
+    @Autowired
+    private MinioUtils minioUtils;
+
+    @Autowired
+    private MinioConfig minioConfig;
+
+    /**
+     * file upload
+     *
+     * @param file
+     */
+    @PostMapping("/upload")
+    public String upload(@RequestParam("file") MultipartFile file) {
+        try {
+            //file name
+            String fileName = file.getOriginalFilename();
+            String newFileName = System.currentTimeMillis() + "." + StringUtils.substringAfterLast(fileName, ".");
+            log.info("newFileName: {}", newFileName);
+            //type
+            String contentType = file.getContentType();
+            minioUtils.uploadFile(minioConfig.getBucketName(), file, newFileName, contentType);
+            return "upload success";
+        } catch (Exception e) {
+            e.printStackTrace();
+            log.error("upload fail");
+            return "upload fail";
+        }
+    }
+
+    /**
+     * delete
+     *
+     * @param fileName
+     */
+    @DeleteMapping("/")
+    public void delete(@RequestParam("fileName") String fileName) {
+        minioUtils.removeFile(minioConfig.getBucketName(), fileName);
+    }
+
+    /**
+     * get file info
+     *
+     * @param fileName
+     * @return
+     */
+    @GetMapping("/info")
+    public String getFileStatusInfo(@RequestParam("fileName") String fileName) {
+        return minioUtils.getFileStatusInfo(minioConfig.getBucketName(), fileName);
+    }
+
+    /**
+     * get file url
+     *
+     * @param fileName
+     * @return
+     */
+    @GetMapping("/url")
+    public String getPresignedObjectUrl(@RequestParam("fileName") String fileName) {
+        return minioUtils.getPresignedObjectUrl(minioConfig.getBucketName(), fileName);
+    }
+
+    /**
+     * file download
+     *
+     * @param fileName
+     * @param response
+     */
+    @GetMapping("/download")
+    public void download(@RequestParam("fileName") String fileName, HttpServletResponse response) {
+        try {
+            InputStream fileInputStream = minioUtils.getObject(minioConfig.getBucketName(), fileName);
+            response.setHeader("Content-Disposition", "attachment;filename=" + fileName);
+            response.setContentType("application/force-download");
+            response.setCharacterEncoding("UTF-8");
+            IOUtils.copy(fileInputStream, response.getOutputStream());
+        } catch (Exception e) {
+            log.error("download fail");
+        }
+    }
+
+}
