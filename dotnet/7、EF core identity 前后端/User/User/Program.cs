fe21b9f9f39a59ebd5318a1a0da0a9d4eca674ed
miku-wwl <569144003@qq.com>
2025-09-22 10:33:11 +0800

    notes: dotnet

--- dotnet/7、EF core identity 前后端/User/User/Program.cs
+++ dotnet/7、EF core identity 前后端/User/User/Program.cs
@@ -37,25 +37,6 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-    {
-        var forecast = Enumerable.Range(1, 5).Select(index =>
-                new WeatherForecast
-                (
-                    DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                    Random.Shared.Next(-20, 55),
-                    summaries[Random.Shared.Next(summaries.Length)]
-                ))
-            .ToArray();
-        return forecast;
-    })
-    .WithName("GetWeatherForecast");
-
 app.UseAuthentication();
 app.UseAuthorization();
 
@@ -80,8 +61,3 @@ catch (Exception ex)
 
 
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
